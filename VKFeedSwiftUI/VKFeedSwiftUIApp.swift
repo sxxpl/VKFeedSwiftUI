d16f153894a1b9ba9bf7a586d849069a7069c4e8
Artem <artem-tikhonov-90@list.ru>
2022-11-06 14:55:47 +0300

    first commit

---
+++ VKFeedSwiftUI/VKFeedSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKFeedSwiftUIApp.swift
+//  VKFeedSwiftUI
+//
+//  Created by Артем Тихонов on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKFeedSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
